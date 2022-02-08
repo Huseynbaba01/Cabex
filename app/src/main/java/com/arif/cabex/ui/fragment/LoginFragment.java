e86230ec55f34652b3e6209f744407b22fc88052
Huseynbaba01 <hrzaquliyev@std.beu.edu.az>
2022-02-08 23:07:53 +0400

    [initial_branch] Navigation has been added with safe args

--- app/src/main/java/com/arif/cabex/ui/fragment/LoginFragment.java
+++ app/src/main/java/com/arif/cabex/ui/fragment/LoginFragment.java
@@ -6,17 +6,19 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.arif.cabex.databinding.FragmentLoginBinding;
 
 import org.jetbrains.annotations.NotNull;
 
-public class LoginFragment extends Fragment implements View.OnClickListener {
+public class LoginFragment extends Fragment {
 	private FragmentLoginBinding binding;
 	private NavDirections direction;
 	private NavController navController;
@@ -25,41 +27,24 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
 	public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
 		binding = FragmentLoginBinding.inflate(inflater, container, false);
-		direction = LoginFragmentDirections.actionLoginFragmentToRegisterFragment();
-		binding.registrationLink.setOnClickListener(this);
-		binding.signInButton.setOnClickListener(this);
-		binding.googleButton.setOnClickListener(this);
+		direction = LoginFragmentDirections.actionLoginFragmentToRegisterFragment("9836");
+		binding.registrationLink.setOnClickListener(this::onRegistrationLinkClicked);
+		binding.signIn.setOnClickListener(this::onSignInButtonClicked);
+		binding.googleButton.setOnClickListener(this::onGoogleButtonClicked);
 
 		return binding.getRoot();
 	}
 
-	@Override
-	public void onClick(View view) {
-		int id = view.getId();
-		if(id == binding.registrationLink.getId()){
-			onRegistrationLinkClicked(view);
-		}
-		else if(id == binding.signInButton.getId()){
-			onSignInButtonClicked(view);
-		}
-		else if(id == binding.googleButton.getId()){
-			onGoogleButtonClicked(view);
-		}
-	}
-
 	private void onRegistrationLinkClicked(View view) {
+		NavHostFragment.findNavController(this).navigate(direction);
 	}
-
 
 	private void onSignInButtonClicked(View view) {
-		if(true){
-			navController.navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment().getActionId());
-
-
-		}
+		NavHostFragment.findNavController(this).navigate(direction);
 	}
 
 	private void onGoogleButtonClicked(View view) {
+		NavHostFragment.findNavController(this).navigate(direction);
 	}
 
 }
