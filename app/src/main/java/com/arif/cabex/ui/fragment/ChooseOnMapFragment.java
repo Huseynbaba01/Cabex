e2a96fe42904e272b3f81ead6a64c4a74b3731b2
Huseynbaba <hrzaquliyev@std.beu.edu.az>
2022-03-24 15:48:54 +0400

    [CBX-31] Choose on map fragment added, has been placed on the main pages for testing

---
+++ app/src/main/java/com/arif/cabex/ui/fragment/ChooseOnMapFragment.java
@@ -0,0 +1,22 @@
+package com.arif.cabex.ui.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.arif.cabex.R;
+
+public class ChooseOnMapFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_choose_on_map, container, false);
+    }
+}
