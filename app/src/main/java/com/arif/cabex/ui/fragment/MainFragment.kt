a65e1fce1394aec3a4795f64ccf719a61d3cb8cb
Huseynbaba <hrzaquliyev@std.beu.edu.az>
2022-03-31 16:07:30 +0400

    [General] Filter dialog has been added

--- app/src/main/java/com/arif/cabex/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/arif/cabex/ui/fragment/MainFragment.kt
@@ -9,6 +9,7 @@ import com.arif.cabex.model.DriverInfo
 import com.arif.cabex.R
 import com.arif.cabex.databinding.FragmentMainBinding
 import com.arif.cabex.ui.adapter.DriverInfoAdapter
+import com.arif.cabex.ui.dialog.FilterDialog
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
@@ -20,6 +21,7 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMainBinding.inflate(inflater)
+        setListeners()
         listOfDriversInfo.add(
             DriverInfo(R.drawable.filter_alt, "Bakı", "Gəncə", "Kərim Əhmədli",
                 "14:00", "Ford Mustang",4,3)
@@ -37,8 +39,16 @@ class MainFragment : Fragment() {
 
     }
 
+    private fun setListeners(){
+        binding.cvImage.setOnClickListener {
+            val dialog = FilterDialog(requireContext())
+            dialog.show()
+        }
+    }
 
+    private fun addMockDrivers(){
 
+    }
     companion object {
 
     }
